294fef54e26832df0700d7f490c5d34c99e141d3
hankkuu <hankkuu@gmail.com>
2020-01-03 15:07:38 +0900

    update project

---
+++ src/main/java/kosta/namtang/talkbook/TalkbookApplication.java
@@ -0,0 +1,13 @@
+package kosta.namtang.talkbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kosta/namtang/talkbook/TalkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package kosta.namtang.talkbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
