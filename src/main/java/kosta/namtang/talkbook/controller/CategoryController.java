b99e31b30ecd029a53d37b67c893edb63a7438fc
Jeongleeyop <anfdmavy777@naver.com>
2020-01-06 18:59:08 +0900

    2020-01-06 Category Ui

---
+++ src/main/java/kosta/namtang/talkbook/controller/CategoryController.java
@@ -0,0 +1,11 @@
+package kosta.namtang.talkbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CategoryController {
+
+    @RequestMapping("aroma/category")
+    void category(){}
+}
