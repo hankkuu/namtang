62ec480c5600170e1be746ad632a2483eea5c6d9
Kang <ket3613@naver.com>
2020-01-10 10:15:05 +0900

    스케줄러 구동확인; 관리자 시큐리티로 구동확인

--- src/main/java/kosta/namtang/talkbook/TalkbookApplication.java
+++ src/main/java/kosta/namtang/talkbook/TalkbookApplication.java
@@ -2,8 +2,10 @@ package kosta.namtang.talkbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TalkbookApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/kosta/namtang/talkbook/repository/StatisticsRepository.java
@@ -0,0 +1,9 @@
+package kosta.namtang.talkbook.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import kosta.namtang.talkbook.model.domain.Statistics;
+
+public interface StatisticsRepository extends CrudRepository<Statistics, Long> {
+
+}
