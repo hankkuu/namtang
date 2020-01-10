1d6375a8f9458068b0ce87c732267b48fbce3b6f
Kang <ket3613@naver.com>
2020-01-10 10:55:57 +0900

    커밋

--- src/main/java/kosta/namtang/talkbook/TalkbookApplication.java
+++ src/main/java/kosta/namtang/talkbook/TalkbookApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @Slf4j
 @EnableScheduling
@@ -11,6 +13,12 @@ public class TalkbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalkbookApplication.class, args);
+
+		log.error("error");
+		log.warn("warn");
+		log.info("info");
+		log.trace("trace");
+		log.debug("debug");
 	}
 
 }
