8d2f2749e1bf0e3f1c76644aa624fb7372c161fc
hankkuu <hankkuu@gmail.com>
2020-01-13 13:26:09 +0900

    update spring security

--- src/main/java/kosta/namtang/talkbook/repository/account/AccountRepository.java
+++ src/main/java/kosta/namtang/talkbook/repository/account/AccountRepository.java
@@ -5,4 +5,5 @@ import kosta.namtang.talkbook.model.domain.account.Account;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    Account findByUserId(String userId);
 }
