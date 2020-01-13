3c2a4abe42fdde2e15bd60db37a8cee4cf758a26
JeongLeeyop <anfdmavy777@naver.com>
2020-01-13 15:51:24 +0900

    2020-01-13 security 비활성화

--- src/main/java/kosta/namtang/talkbook/security/SimpleUserService.java
+++ src/main/java/kosta/namtang/talkbook/security/SimpleUserService.java
@@ -1,26 +1,26 @@
-package kosta.namtang.talkbook.security;
-
-import kosta.namtang.talkbook.model.domain.account.Account;
-import kosta.namtang.talkbook.repository.account.AccountRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-@Slf4j
-public class SimpleUserService implements UserDetailsService {
-
-    @Autowired
-    AccountRepository accountRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
-        Account acc = accountRepository.findByUserId(id);
-        SimpleSecurityUser user = new SimpleSecurityUser(acc);
-
-        return user;
-    }
-}
+//package kosta.namtang.talkbook.security;
+//
+//import kosta.namtang.talkbook.model.domain.account.Account;
+//import kosta.namtang.talkbook.repository.account.AccountRepository;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@Slf4j
+//public class SimpleUserService implements UserDetailsService {
+//
+//    @Autowired
+//    AccountRepository accountRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+//        Account acc = accountRepository.findByUserId(id);
+//        SimpleSecurityUser user = new SimpleSecurityUser(acc);
+//
+//        return user;
+//    }
+//}
